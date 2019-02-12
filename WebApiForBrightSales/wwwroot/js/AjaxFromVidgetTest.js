3bef23b4bf68a352f0c4b68be3ac4b6890a77fdc
Simon Westberg <simme.westberg@hotmail.com>
2019-02-12 16:59:22 +0100

    more work

--- WebApiForBrightSales/wwwroot/js/AjaxFromVidgetTest.js
+++ WebApiForBrightSales/wwwroot/js/AjaxFromVidgetTest.js
@@ -1,4 +1,13 @@
 ﻿
-    $("#buttonId").click(function () {
-        alert("TESTONCLICK");
-    });
+
+    function getClicks() {
+        console.log("TESTFROM GET CLICK FUNCTION");
+
+        $("#buttonId").on('click', function () {
+            alert("TESTONCLICK");
+        })();
+
+
+    }
+
+    getClicks();
